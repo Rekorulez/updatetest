c8414bb11ba3eb307de2c28146d5913ba6c4cc9c
Rekorulez <40898567+Rekorulez@users.noreply.github.com>
2018-07-09 19:13:57 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -6,7 +6,10 @@ import json
 while 1:
     nuvem = requests.get("https://raw.githubusercontent.com/Rekorulez/updatetest/master/ver.json")
     disco = ver.get("Versões atuais")
+    print(nuvem)
+    print(disco)
     
+    sleep(10)
     if  disco == nuvem:
         print("atualizado")
     else:
