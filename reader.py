9465360072e11951170c907454e9f78baa2dceba
Rekorulez <40898567+Rekorulez@users.noreply.github.com>
2018-07-09 18:14:44 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,4 +1,13 @@
 handle = open("ver", "r")
 data = handle.read()
-print(data)
+handle2 = open("/home/pi/ver", "r")
+data2 = handle2.read()
+
+if data == data2
+    print("atualizado")
+else
+    print("desatualizado")
+
 handle.close
+handle2.close
+
