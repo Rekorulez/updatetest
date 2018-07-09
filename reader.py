b3d1eb4df1a05411e3d3d9cf1e5f48e827ec04a0
Rekorulez <40898567+Rekorulez@users.noreply.github.com>
2018-07-09 18:40:12 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,13 +1,22 @@
-handle = open("ver", "r")
-data = handle.read()
-handle2 = open("/home/pi/ver", "r")
-data2 = handle2.read()
+import time
+import os
 
-if data == data2:
-    print("atualizado")
-else:
-    print("desatualizado")
 
-handle.close
-handle2.close
+while true:
+    os.system('sudo rm /home/pi/ver')
+    os.system('sudo wget https://raw.githubusercontent.com/Rekorulez/updatetest/master/ver')
+    handle = open("ver", "r")
+    data = handle.read()
+    handle2 = open("/home/pi/ver", "r")
+    data2 = handle2.read()
 
+    if data == data2:
+        print("atualizado")
+    else:
+        print("desatualizado")
+        os.system('sudo python3 /home/pi/teste.py')
+        break
+
+    handle.close
+    handle2.close
+    time.sleep(60)
