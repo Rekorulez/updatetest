577b04cea214818959fb98aafb3a6824c7b25a3e
Rekorulez <40898567+Rekorulez@users.noreply.github.com>
2018-07-09 19:00:09 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,25 +1,19 @@
 import time
 import os
+import requests
 
-i = 1
-while i == 1:
-    os.system('sudo rm /home/pi/ver')
-    os.system('sudo wget https://raw.githubusercontent.com/Rekorulez/updatetest/master/ver')
-    handle = open("ver", "r")
-    data = handle.read()
-    handle2 = open("/home/pi/ver", "r")
-    data2 = handle2.read()
-    print(data)
-    print(data2)
-    time.sleep(5)
 
-    if data == data2:
+while 1:
+    nuvem = requests.get('https://raw.githubusercontent.com/Rekorulez/updatetest/master/ver.json')
+    disco = open("ver.json")
+    dd = disco.read()
+    
+    if  dd == nuvem:
         print("atualizado")
     else:
         print("desatualizado")
         os.system('sudo python3 /home/pi/teste.py')
-        i = 0
+        break
 
-    handle.close
-    handle2.close
+
     time.sleep(60)
