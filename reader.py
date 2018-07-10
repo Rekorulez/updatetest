441f2f8951eae7447ff3daabebccd8ad3c0b1709
Rekorulez <40898567+Rekorulez@users.noreply.github.com>
2018-07-10 10:43:58 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -5,18 +5,19 @@ import json
 
 while 1:
     nuvem = requests.get("https://raw.githubusercontent.com/Rekorulez/updatetest/master/ver.json")
-    disco = json.loads('/home/pi/updatetest/ver.json')
+    disco = open("/home/pi/updatetest/ver.json", "r").read()
+    d = json.loads(disco)
     
     print(nuvem.content)
-    print(disco)
+    print(d)
     
     sleep(10)
-    if  disco == nuvem:
-        print("atualizado")
-    else:
-        print("desatualizado")
-        os.system('sudo python3 /home/pi/teste.py')
-        break
+    #if  disco == nuvem:
+    #    print("atualizado")
+    #else:
+    #    print("desatualizado")
+    #    os.system('sudo python3 /home/pi/teste.py')
+    #    break
 
 
     time.sleep(60)
